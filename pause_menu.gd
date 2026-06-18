530664992508876d9575aeb88dcbcc3838e594f1
Umbrakinetic <quinnspirit@gmail.com>
2026-06-17 18:44:16 -0700

    added basic pause button functionality

--- pause_menu.gd
+++ pause_menu.gd
@@ -19,6 +19,17 @@ func _unhandled_input(event: InputEvent) -> void:
 func _on_resume_button_pressed() -> void:
 	resume()
 
+func _on_restart_button_pressed() -> void:
+	resume()
+	get_tree().change_scene_to_file(get_tree().current_scene.scene_file_path)
+
+func _on_main_menu_button_pressed() -> void:
+	resume()
+	get_tree().change_scene_to_file("res://main_menu/main_menu.tscn")
+
+func _on_desktop_button_pressed() -> void:
+	get_tree().quit()
+
 func resume():
 	hide()
 	release_focus()
